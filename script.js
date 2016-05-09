923d0157a7c209f4e2a76a90c6762f592d06f9cb
guillaumesalomon <guillaume@videodesk.com>
2016-05-10 00:15:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Alors comme ça tu veux en savoir plus sur ajstage ? C'est juste un petit message pour dire bonjour')
                 .then(() => 'speak');
         }
     },
